db25b4279b480fc3d505d544ee415849c743f4fc
Mihail Kuznetsov <misha_kuznetsov@ukr.net>
2012-01-26 16:44:50 +0200

    Working index page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,41 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+  describe "GET 'index'" do
+
+     it "should deny access " do
+        get :index
+        response.should redirect_to(signin_path)
+     end
+
+    describe "for signed-in-users" do
+
+      before(:each)do
+        @user = test_sign_in(Factory(:user))
+        Factory(:user, :email => "user@chubaka.net")
+        Factory(:user, :email => "user@karton.net")
+      end
+
+      it "should be successful" do
+        get :index
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get :index
+        response.should have_selector('title', :content => "All users")
+      end
+
+      it "should have an element for each user" do
+        get :index
+        User.all.each do |user|
+          response.should have_selector('li', :content => user.name)
+        end
+      end
+
+    end
+  end
+
   describe "GET 'show'" do
 
     before(:each) do
