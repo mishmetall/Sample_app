ffd9f2ecd4755713a469b137b783800d96ba8abb
Mihail Kuznetsov <misha_kuznetsov@ukr.net>
2012-01-28 23:50:55 +0200

    Finishd microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,18 +1,21 @@
 class MicropostsController < ApplicationController
   before_filter :authenticate
+  before_filter :authorized_user, :only => :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
     if @micropost.save
       redirect_to root_path, :flash => { :success => "Micropost created!" }
     else
+      @feed_items = []
       render 'pages/home'
     end
 
   end
 
   def destroy
-
+    @micropost.destroy
+    redirect_to root_path, :flash => { :success => "Micropost deleted" }
   end
 
   private
@@ -21,4 +24,8 @@ class MicropostsController < ApplicationController
       deny_access unless signed_in?
     end
 
+    def authorized_user
+      @micropost = Micropost.find(params[:id])
+      redirect_to root_path unless current_user?(@micropost.user)
+    end
 end
